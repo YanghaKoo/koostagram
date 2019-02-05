e8c73e10eb83a6ff33e051b8222c6c43cc7c616b
koo <yangha93@naver.com>
2019-02-05 19:55:00 +0900

    0205 fix bug#2

---
+++ sns-backend/public/precache-manifest.0ad21340df2e0412da274e9264d8eca6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "4597a1a074ad897d087d86336ff1fe64",
+    "url": "/static/media/puff.4597a1a0.svg"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "57a8a25cad71adf5cdf8",
+    "url": "/static/js/main.57a8a25c.chunk.js"
+  },
+  {
+    "revision": "6d104bebe3bd34fa9376",
+    "url": "/static/js/1.6d104beb.chunk.js"
+  },
+  {
+    "revision": "57a8a25cad71adf5cdf8",
+    "url": "/static/css/main.a84b1465.chunk.css"
+  },
+  {
+    "revision": "927aebde3032ec54ad399ecf66c0f7d5",
+    "url": "/index.html"
+  }
+];
